a16fc5b709a491796fb84477a06219d91f13253e
Savij Apichon <gunnapichon@gmail.com>
2023-02-13 14:14:16 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
+# import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__)
 server = app.server
@@ -34,7 +34,7 @@ def feature_bar():
     return feature_bar
 
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+# app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 # ---------------------------------------------------------------
 app.layout = html.Div([
 
